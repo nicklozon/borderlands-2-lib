45a1fa8fad3bb2d047594d401bea9d21839e3caf
Nick Lozon <nicklozon@gmail.com>
2020-03-21 15:48:16 -0400

    giving up, gunzerker is complicated

---
+++ src/domain/player/object/skills/gunzerker.ts
@@ -0,0 +1,98 @@
+import { Skill } from "../skill";
+import { StatType } from "../../value_object/stat_type";
+import { Stat } from "../../interface/stat";
+import { Weapon } from "../../../weapon/interface/weapon";
+import { Type } from "../../../weapon/value_object/type";
+
+// Gunlust
+export class LockedAndLoaded extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.FireRate,
+    value: 0.05
+  }]
+}
+
+export class QuickDraw extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.CritHitDamage,
+    value: 0.02
+  }]
+}
+
+export class ImYourHuckleberry extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.GunDamage,
+    value: 0.03
+  },{
+    type: StatType.ReloadSpeed,
+    value: 0.03
+  }]
+
+  public getStat(statType: StatType, weapon: Weapon): number {
+    if(weapon.type !== Type.Pistol) return 0
+
+    return super.getStat(statType, weapon)
+  }
+}
+
+export class AllINeedIsOne extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.GunDamage,
+    value: 0.08
+  }]
+}
+
+export class DivergentLikeness extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.GunDamage,
+    value: 0.06
+  }]
+}
+
+export class MoneyShot extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.GunDamage,
+    value: 0.08
+  }]
+
+  public getStat(statType: StatType, weapon: Weapon): number {
+    let stat = super.getStat(statType, weapon) * weapon.magazineSize
+
+    return stat > 0.8 ? 0.8 : stat
+  }
+}
+
+export class LayWaste extends Skill {
+  protected stats: Stat[] = [{
+      type: StatType.FireRate,
+      value: 0.08
+    },{
+      type: StatType.CritHitDamage,
+      value: 0.05
+    }]
+
+  public getStat(statType: StatType, weapon: Weapon): number {
+    if(weapon.type !== Type.RocketLauncher) return 0
+
+    return super.getStat(statType, weapon)
+  }
+}
+
+export class KeepItPipingHot extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.GunDamage,
+    value: 0.05
+  }]
+}
+
+// Rampage
+export class Inconceivable extends Skill {
+  protected stats: Stat[] = [{
+    type: StatType.MagazineSize,
+    value: 0.10
+  }]
+
+  // We need to know the health and shield of the player to know the %
+}
+
+// Brawn
